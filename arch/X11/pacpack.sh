5d42137029884aec95b67d4e66ea37256cff81cf
gregborane <madaradellac@gmail.com>
2025-11-17 01:05:01 +0100

    bootloader theme update

--- arch/X11/pacpack.sh
+++ arch/X11/pacpack.sh
@@ -4,31 +4,31 @@
 sudo pacman -Syu
 
 # Dependencies for my System
-sudo pacman -S --noconfirm --needed alacritty alsa-plugins aspell atril
+sudo pacman -S --noconfirm --needed ghostty aspell atril
 sudo pacman -S --noconfirm --needed base-devel bash-completion biber
 sudo pacman -S --noconfirm --needed cmake composer curl
 sudo pacman -S --noconfirm --needed dkms dotnet-runtime-7.0 dotnet-runtime-8.0
 #sudo pacman -S --nocnfirm --needed e
-sudo pacman -S --noconfirm --needed feh ffmpeg flameshot fontconfig fuse-common fzf
-sudo pacman -S --noconfirm --needed gamescope gimp giflip gnome-text-editor ghostscript git gtk3 gtk4
+sudo pacman -S --noconfirm --needed ffmpeg fontconfig fuse-common fzf
+sudo pacman -S --noconfirm --needed gamescope gamemode gimp giflip gnome-text-editor ghostscript git gtk3 gtk4
 sudo pacman -S --noconfirm --needed hdf5 hspell htop
-sudo pacman -S --noconfirm --needed i3lock imagemagick
+sudo pacman -S --noconfirm --needed imagemagick
 sudo pacman -S --noconfirm --needed jdk-openjdk julia
 #sudo pacman -S --noconfirm --needed k
-sudo pacman -S --noconfirm --needed less libreoffice-fresh linux-lts linux-lts-docs linux-lts-headers luarocks lutris
+sudo pacman -S --noconfirm --needed less libreoffice-fresh luarocks lutris
 sudo pacman -S --noconfirm --needed mariadb mermaid-cli
 sudo pacman -S --noconfirm --needed ncurses neovim ninja noto-fonts-emoji nuspell nwg-look
 sudo pacman -S --noconfirm --needed obs-studio openal openssh
-sudo pacman -S --noconfirm --needed pavucontrol php picom python-neovim
+sudo pacman -S --noconfirm --needed pavucontrol php python-neovim
 sudo pacman -S --noconfirm --needed qt5 qt6
-sudo pacman -S --noconfirm --needed r ripgrep rofi rofi-calc ruby
-sudo pacman -S --noconfirm --needed sddm sqlite sshfs steam
+sudo pacman -S --noconfirm --needed r ripgrep ruby
+sudo pacman -S --noconfirm --needed sqlite sshfs steam starship
 sudo pacman -S --noconfirm --needed tectonic texlive thunar timeshift tree-sitter-cli thunderbird
 sudo pacman -S --noconfirm --needed ueberzugpp upower unzip
 sudo pacman -S --noconfirm --needed vlc vlc-plugin-ffmpeg
 sudo pacman -S --noconfirm --needed wget wine-staging
-sudo pacman -S --noconfirm --needed xorg-mkfontscale
-#sudo pacman -S --noconfirm --needed y
+#sudo pacman -S --noconfirm --needed x
+sudo pacman -S --noconfirm --needed yay
 sudo pacman -S --noconfirm --needed zathura zip zoxide
 
 # lib* packages (alphabetically sorted)
@@ -43,9 +43,3 @@ vulkan-icd-loader vulkan-tools vulkan-validation-layers \
 mesa vulkan-radeon vulkan-intel vulkan-nouveau \
 lib32-mesa lib32-vulkan-radeon lib32-vulkan-intel lib32-vulkan-nouveau \
 lib32-vulkan-icd-loader lib32-vulkan-validation-layers
-
-# Install oh-my-posh
-read -p "install OMP? [y,N]" $OMP
-if [["$OMP" =~ ^(%y|Y||[Yy]es)$ ]]; then
-  curl -s https://ohmyposh.dev/install.sh | bash -s
-fi
