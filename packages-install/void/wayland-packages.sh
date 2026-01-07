21ffb952404649b8b3e79e8631d4ad14d145f69d
gregborane <madaradellac@gmail.com>
2026-01-07 11:51:44 +0100

    vulkan and nvidia stuffs for void installation

--- packages-install/void/wayland-packages.sh
+++ packages-install/void/wayland-packages.sh
@@ -19,16 +19,18 @@ sudo xbps-install openjdk openjdk-common juliaup
 # sudo xbps-install k
 sudo xbps-install lazygit less libreoffice-fresh linux-lts linux-lts-docs linux-lts-headers luarocks lutris
 sudo xbps-install mako mariadb
-sudo xbps-install ncurses neovim ninja noto-fonts-ttf noto-fonts-emojis noto-fonts-cjk-sans nuspell
+sudo xbps-install ncurses nvidia neovim ninja noto-fonts-ttf noto-fonts-emojis noto-fonts-cjk-sans nuspell NetworkManager
 sudo xbps-install obs openssh libopenal
-sudo xbps-install pavucontrol polkit-gnome php python3-neovim plymouth
+sudo xbps-install pavucontrol polkit-gnome php python3-neovim plymouth protontricks
 sudo xbps-install qt5 qt6 qbittorrent
 sudo xbps-install r ripgrep ruby
 sudo xbps-install sqlite starship StyLua
 sudo xbps-install tectonic texlive thunar timeshift tree-sitter thunderbird
 sudo xbps-install ufw upower unzip
 sudo xbps-install vlc libvlc 
-sudo xbps-install waybar wiremix wget wine wine-gecko wine-mono wine-tools protontricks wineasio winetricks wofi
-sudo xbps-install xdotool 
+sudo xbps-install waybar wiremix wget wine wine-gecko wine-mono wine-tools wineasio winetricks wofi wayland
+sudo xbps-install xdotool xorg-server-xwayland
 # sudo xbps-install y
 sudo xbps-install zathura zip zoxide
+
+sudo xbps-install Vulkan-Headers Vulkan-Tools Vulkan-Utility-Libraries Vulkan-Utility-Libraries-32bit	Vulkan-ValidationLayers Vulkan-ValidationLayers-32bit vulkan-loader vulkan-loader-32bit
