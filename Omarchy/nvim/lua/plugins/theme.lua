f524692e5352c354366125465e7a50addaedef3f
gregborane <madaradellac@gmail.com>
2026-01-13 16:39:22 +0100

    adding new files

--- Omarchy/nvim/lua/plugins/theme.lua
+++
@@ -1,1 +0,0 @@
-/home/greg/.config/omarchy/current/theme/neovim.lua
--- dotfiles/X11/autostart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-picom --config "$HOME/.config/picom/picom.conf" &
-feh --bg-center "$HOME/.config/wallpaper.png" &
-
