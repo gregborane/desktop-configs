ff5550db5647d557b737462ea183f2f5f3f3835f
gregborane <madaradellac@gmail.com>
2026-09-25 14:03:54 -0400

    nvim lock file

--- install_files/nix/nixos-post/home.nix
+++ install_files/nix/nixos-post/home.nix
@@ -102,9 +102,7 @@
 
     programs.neovim = {
         enable = true;
-        package =
-            inputs.neovim-nightly-overlay.packages
-            .${pkgs.stdenv.hostPlatform.system}.default;
+        package = inputs.neovim-nightly-overlay.packages.${pkgs.stdenv.hostPlatform.system}.default;
         defaultEditor = true;
         };
 
