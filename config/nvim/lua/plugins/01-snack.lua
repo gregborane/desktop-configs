e01a2158fad5ef842019d52a468b61979de5634c
gregborane <madaradellac@gmail.com>
2026-09-25 16:10:52 -0400

    tracking changed

--- config/nvim/lua/plugins/01-snack.lua
+++ config/nvim/lua/plugins/01-snack.lua
@@ -1,48 +1,82 @@
--- Snacks: loaded early (01- prefix) so its picker is available to other plugins.
+-- Snacks: loaded early so its picker is available to other plugins.
 vim.pack.add({
   {
     src = "https://github.com/nvim-tree/nvim-web-devicons",
     version = "master",
   },
-  "https://github.com/folke/snacks.nvim",
+  {
+    src = "https://github.com/folke/snacks.nvim",
+  },
 })
 
 ---@type snacks.Config
 require("snacks").setup({
-  bigfile = { enabled = true },
+  bigfile = {
+    enabled = true,
+  },
 
-  explorer = { enabled = true },
+  explorer = {
+    enabled = true,
+  },
 
-  indent = { enabled = true },
+  indent = {
+    enabled = true,
+  },
 
-  input = { enabled = true },
+  input = {
+    enabled = true,
+  },
 
-  picker = { enabled = true },
+  notifier = {
+    enabled = true,
+  },
 
-  notifier = { enabled = true },
+  quickfile = {
+    enabled = true,
+  },
 
-  quickfile = { enabled = true },
+  scope = {
+    enabled = true,
+  },
 
-  scope = { enabled = true },
+  scroll = {
+    enabled = true,
+  },
 
-  scroll = { enabled = true },
+  statuscolumn = {
+    enabled = true,
+  },
 
-  statuscolumn = { enabled = true },
+  words = {
+    enabled = true,
+  },
 
-  words = { enabled = true },
+  image = {
+    enabled = true,
+  },
 
-  image = { enabled = true },
+  zen = {
+    enabled = true,
+  },
 
-  zen = { enabled = true },
+  picker = {
+    enabled = true,
 
-  project = {
-    dirs = {
-      "~/nixos-dotfiles/desktop-configs/config/nvim",
-      "~/Work",
+    sources = {
+      projects = {
+        projects = {
+          "~/nixos-dotfiles/desktop-configs/config/nvim",
+          "~/Work",
+        },
+
+        recent = true,
+      },
     },
   },
 
   dashboard = {
+    enabled = true,
+
     preset = {
       header = [[
 ██   ██╗██╗██╗
@@ -86,6 +120,12 @@ require("snacks").setup({
           action = ":lua Snacks.dashboard.pick('files', { cwd = vim.fn.stdpath('config') })",
         },
         {
+          icon = " ",
+          key = "p",
+          desc = "Projects",
+          action = ":lua Snacks.picker.projects()",
+        },
+        {
           icon = " ",
           key = "h",
           desc = "Health",
@@ -99,85 +139,156 @@ require("snacks").setup({
         },
       },
     },
+
+    -- Explicitly replace the defaults.
+    -- Do NOT add { section = "startup" } here.
+    -- That section calls lazy.stats and requires lazy.nvim.
+    sections = {
+      {
+        section = "header",
+      },
+      {
+        section = "keys",
+        gap = 1,
+        padding = 1,
+      },
+    },
   },
 })
 
 local map = vim.keymap.set
 
+-- Search help
 map("n", "<leader>sh", function()
   Snacks.picker.help()
-end, { desc = "[S]earch [H]elp" })
+end, {
+  desc = "[S]earch [H]elp",
+})
 
+-- Search keymaps
 map("n", "<leader>sk", function()
   Snacks.picker.keymaps()
-end, { desc = "[S]earch [K]eymaps" })
+end, {
+  desc = "[S]earch [K]eymaps",
+})
 
+-- Search commands
 map("n", "<leader>sc", function()
   Snacks.picker.commands()
-end, { desc = "[S]earch [C]ommands" })
+end, {
+  desc = "[S]earch [C]ommands",
+})
 
+-- Search Snacks builtins
 map("n", "<leader>sb", function()
   Snacks.picker.builtin()
-end, { desc = "[S]earch [B]uiltins" })
+end, {
+  desc = "[S]earch [B]uiltins",
+})
 
+-- Find files
 map("n", "<leader>sf", function()
   Snacks.picker.files()
-end, { desc = "[S]earch [F]iles" })
+end, {
+  desc = "[S]earch [F]iles",
+})
 
+-- File explorer
 map("n", "<leader>fe", function()
   Snacks.explorer.open()
-end, { desc = "[F]iles [E]xplorer open tree" })
+end, {
+  desc = "[F]iles [E]xplorer",
+})
 
+-- Smart file picker
 map("n", "<leader>ff", function()
   Snacks.picker.smart()
-end, { desc = "[S]earch [F]iles" })
+end, {
+  desc = "[F]ind [F]iles",
+})
 
+-- Select Snacks picker
 map("n", "<leader>ss", function()
   Snacks.picker.pickers()
-end, { desc = "[S]earch [S]elect Snacks" })
+end, {
+  desc = "[S]earch [S]elect Snacks",
+})
 
+-- Search current word / visual selection
 map({ "n", "x" }, "<leader>sw", function()
   Snacks.picker.grep_word()
-end, { desc = "[S]earch current [W]ord" })
+end, {
+  desc = "[S]earch current [W]ord",
+})
 
+-- Grep files
 map("n", "<leader>fg", function()
   Snacks.picker.grep()
-end, { desc = "[S]earch by [G]rep" })
+end, {
+  desc = "[F]ind by [G]rep",
+})
 
+-- Projects
 map("n", "<leader>fp", function()
   Snacks.picker.projects()
-end, { desc = "[S]earch [P]rojects" })
+end, {
+  desc = "[F]ind [P]rojects",
+})
 
+-- Diagnostics
 map("n", "<leader>sd", function()
   Snacks.picker.diagnostics()
-end, { desc = "[S]earch [D]iagnostics" })
+end, {
+  desc = "[S]earch [D]iagnostics",
+})
 
+-- Resume last picker
 map("n", "<leader>sr", function()
   Snacks.picker.resume()
-end, { desc = "[S]earch [R]esume" })
+end, {
+  desc = "[S]earch [R]esume",
+})
 
+-- Recent files
 map("n", "<leader>s.", function()
   Snacks.picker.recent()
-end, { desc = '[S]earch Recent Files ("." for repeat)' })
+end, {
+  desc = "[S]earch Recent Files",
+})
 
+-- Existing buffers
 map("n", "<leader><leader>", function()
   Snacks.picker.buffers()
-end, { desc = "[ ] Find existing buffers" })
+end, {
+  desc = "Find existing buffers",
+})
 
+-- Search current buffer
 map("n", "<leader>/", function()
   Snacks.picker.lines()
-end, { desc = "[/] Fuzzily search in current buffer" })
+end, {
+  desc = "Fuzzily search current buffer",
+})
 
+-- Search open buffers
 map("n", "<leader>s/", function()
   Snacks.picker.grep_buffers()
-end, { desc = "[S]earch [/] in Open Files" })
+end, {
+  desc = "[S]earch in Open Files",
+})
 
+-- Search Neovim config
 map("n", "<leader>sn", function()
   Snacks.picker.files({
     cwd = vim.fn.stdpath("config"),
   })
-end, { desc = "[S]earch [N]eovim files" })
+end, {
+  desc = "[S]earch [N]eovim files",
+})
 
+-- Zen mode
 map("n", "<C-\\>", function()
   Snacks.zen()
-end, { desc = "Toggle [Z]en mode" })
+end, {
+  desc = "Toggle Zen mode",
+})
